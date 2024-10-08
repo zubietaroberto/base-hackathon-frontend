8a06164cb3e3a2810d4bc2731eced7b8e3a8b3d7
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-10-07 22:21:39 -0500

    Downloading Credit-Issued

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -10,9 +10,24 @@ interface TransferEvent {
   blockNumber?: string;
 }
 
+interface CreditIssuedEvent {
+  to?: string;
+  tokenId?: string;
+  principal?: string;
+  totalRepaymentAmount?: string;
+  issuanceDate?: string;
+  creditTerm?: string;
+  blockNumber?: string;
+}
+
 const CONTRACT_ADDRESS = "0xcF8D5F4a495d94B6A5e2a9E43A909B9631F56247";
 const FROM_BLOCK: bigint = 21808180n;
 const TO_BLOCK: bigint = 23672246n;
+
+const supabaseClient = createClient(
+  process.env.SUPABASE_URL ?? "",
+  process.env.SUPABASE_KEY ?? ""
+);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -30,11 +45,6 @@ task(
   "download-transfers",
   "Downloads existing transfers from the blockchain",
   async (_, hre) => {
-    const supabase = createClient(
-      process.env.SUPABASE_URL ?? "",
-      process.env.SUPABASE_KEY ?? ""
-    );
-
     const contract = await hre.viem.getContractAt(
       "RodaCreditCOP",
       CONTRACT_ADDRESS
@@ -57,7 +67,48 @@ task(
           tokenId: event.args.tokenId?.toString(),
           blockNumber: event.blockNumber.toString(),
         };
-        promises.push(supabase.from("Transfer").insert(parsedEvent));
+        promises.push(supabaseClient.from("Transfer").insert(parsedEvent));
+        events.push(parsedEvent);
+      }
+      await Promise.all(promises);
+
+      console.log("Downloaded", events.length, "events");
+    }
+
+    console.log("Done");
+  }
+);
+
+task(
+  "download-credit-issued",
+  "Downloads existing payments from the blockchain",
+  async (_, hre) => {
+    const contract = await hre.viem.getContractAt(
+      "RodaCreditCOP",
+      CONTRACT_ADDRESS
+    );
+
+    // Iterate over the events since the FROM_BLOCK
+    let events: CreditIssuedEvent[] = [];
+    for (let i = FROM_BLOCK; i < TO_BLOCK; i += 1000n) {
+      console.log("Downloading events from block", i.toString());
+      const newEvents = await contract.getEvents.CreditIssued(
+        {},
+        { fromBlock: i, toBlock: i + 1000n }
+      );
+
+      const promises = [];
+      for (const event of newEvents) {
+        const parsedEvent: CreditIssuedEvent = {
+          to: event.args.to?.toString(),
+          tokenId: event.args.tokenId?.toString(),
+          principal: event.args.principal?.toString(),
+          totalRepaymentAmount: event.args.totalRepaymentAmount?.toString(),
+          issuanceDate: event.args.issuanceDate?.toString(),
+          creditTerm: event.args.creditTerm?.toString(),
+          blockNumber: event.blockNumber.toString(),
+        };
+        promises.push(supabaseClient.from("Credit Issued").insert(parsedEvent));
         events.push(parsedEvent);
       }
       await Promise.all(promises);
