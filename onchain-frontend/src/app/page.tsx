b161fca50641b80aa3c2fb2b879576baca0216a3
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-10-11 20:13:39 -0500

    Basic Supabase integration

--- onchain-frontend/src/app/page.tsx
+++ onchain-frontend/src/app/page.tsx
@@ -2,11 +2,23 @@
 
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import styles from "./page.module.css";
+import { createClient } from "@supabase/supabase-js";
+import { TransferEvent } from "@/types";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL ?? "",
+  process.env.SUPABASE_KEY ?? ""
+);
 
 export default async function Home() {
+  const nfts = await supabase
+    .from<"Transfer", TransferEvent>("Transfer")
+    .select("*")
+    .limit(100);
+
   return (
     <main className={styles.container}>
-      <AppBar>
+      <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">OnchainBrowser</Typography>
           <Button color="inherit">Browse</Button>
@@ -15,6 +27,18 @@ export default async function Home() {
           <Button color="inherit">Issued</Button>
         </Toolbar>
       </AppBar>
+
+      <section className={styles.listContainer}>
+        {nfts.data &&
+          nfts.data.map((nft: TransferEvent) => (
+            <div key={nft.tokenId}>
+              <h1>{nft.tokenId}</h1>
+              <p>
+                {nft.from} -{">"} {nft.to}
+              </p>
+            </div>
+          ))}
+      </section>
     </main>
   );
 }
